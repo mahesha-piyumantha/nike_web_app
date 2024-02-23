1b2663338634ca2822259dc87870963d47f882fb
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-02-24 02:57:16 +0530

    starting app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ export default function, if there are no any console.logs or function callings
 
 */
 
-
 const App = () => (
-
-  <main>
-
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">Super Quality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">Special Offer</section>
   </main>
 );
 
