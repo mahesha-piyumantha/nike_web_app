fa66970b253a2f14743b127b8349aa4f67dfca55
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-02-27 03:24:58 +0530

    started creating nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import {
   SuperQuality,
 } from "./sections";
 
+import Nav from "./components/Nav";
+
 /* 
 This method can only be used instead of using
 export default function, if there are no any console.logs or function callings
@@ -19,15 +21,31 @@ export default function, if there are no any console.logs or function callings
 
 const App = () => (
   <main className="relative">
-    Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-    <section className="padding">Popular Products</section>
-    <section className="padding">Super Quality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">Special Offer</section>
-    <section className="padding bg-pale-blue">Customer Reviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="padding-x padding-t pb-8 bg-black">Footer</section>
+    <Nav/>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      <Footer />
+    </section>
   </main>
 );
 
