083bc5da9ebc418c0ecbf465ae8f9861544e79d3
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-02-28 07:23:57 +0530

    hero page completed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,8 @@
 import { arrowRight } from "../assets/icons";
+import { bigShoe1 } from "../assets/images";
 import Button from "../components/Button";
+import ShoeCard from "../components/ShoeCard";
+import { shoes, statistics } from "../constants";
 
 const Hero = () => {
   return (
@@ -12,17 +15,67 @@ const Hero = () => {
         className="relative xl:w-2/5 flex flex-col justify-center 
       items-start w-full max-xl:padding-x pt-28"
       >
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1
+          className="mt-10 font-palanquin text-8xl max-sm:text-[72px] 
+        max-sm:leading-[82px] font-bold"
+        >
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span> Nike </span>Shoes
+          <span className="text-coral-red inline-block mt-3"> Nike </span> Shoes
         </h1>
-        <p>
+        <p
+          className="font-montserrat text-slate-gray text-lg
+         leading-8 mt-6 mb-14 sm:max-w-sm"
+        >
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
-        <Button label="Shop Now" iconURL={arrowRight}/>
+        <Button label="Shop Now" iconURL={arrowRight} />
+        <div
+          className="flex justify-start items-start
+         flex-wrap w-full mt-20 gap-16"
+        >
+          {statistics.map((stat) => {
+            return (
+              <div key={stat.label}>
+                <p className="text-4xl font-palanquin font-bold">
+                  {stat.value}
+                </p>
+                <p className="leading-7 font-montserrat text-slate-gray">
+                  {stat.label}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div
+        className="relative flex-1 flex justify-center items-center
+       xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center"
+      >
+        <img
+          src={bigShoe1}
+          alt="show collection"
+          className="object-contain relative z-10"
+          width={610}
+          height={500}
+        />
+
+        <div>
+          {shoes.map((shoe) => {
+            return (
+              <div key={shoe}>
+               {/* <ShoeCard/> */}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
