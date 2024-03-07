87597807279c7fbe4e0d2a221714a0e5ef3c5613
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-03-07 21:41:51 +0530

    super quality changes

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,11 @@
 
 const SuperQuality = () => {
   return (
-    <section>
-       SuperQuality
+    <section id="about-us" className="flex justify-between items-center
+     max-lg:flex-col gap-10 w-full max-container">
+      <div className="">
+        <p>We Provide You <span className="text-coral-red">Super Quality</span> Shoes</p>
+       </div>
     </section>
   )
 }
