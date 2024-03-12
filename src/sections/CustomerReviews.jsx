26c13c5b1615bbd4a90ccc680fc4b56b6462ae4b
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-03-12 13:35:38 +0530

    customer reviews section done

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,10 +1,24 @@
+import ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants";
 
 const CustomerReviews = () => {
   return (
     <section className="max-container">
-      CustomerReviews
-    </section>
-  )
-}
+      <h3 className="font-palanquin text-4xl text-center font-bold">
+        What our <span className="text-coral-red">Customers</span> Say?
+      </h3>
 
-export default CustomerReviews
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => {
+          return <ReviewCard key={review.customerName} {...review} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default CustomerReviews;
