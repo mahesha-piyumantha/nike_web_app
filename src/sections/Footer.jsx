aaae7ad5c7b13f61a7b121250dca041956357dad
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-03-13 14:29:44 +0530

    footer finished and Project ended

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,90 @@
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="max-container">
+      <div
+        className="flex justify-between items-start gap-20
+       flex-wrap max-lg:flex-col"
+      >
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} width={150} height={46} />
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+            Get Shoes ready for the new term at your nearest Nike store. Find
+            Your perfect size In Store. Get Rewards.
+          </p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => {
+              return (
+                <div
+                  className="bg-white flex justify-center
+                 items-center w-12 h-12 rounded-full"
+                >
+                  <img
+                    key={icon.src}
+                    src={icon.src}
+                    alt={icon.alt}
+                    width={24}
+                    height={24}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((footerLink) => {
+            return (
+              <div key={footerLink}>
+                <h4
+                  className="text-white font-montserrat text-2xl 
+                  leading-normal font-medium mb-6"
+                >
+                  {footerLink.title}
+                </h4>
+                <ul>
+                  {footerLink.links.map((link) => {
+                    return (
+                      <li
+                        key={link.name}
+                        className="mt-3 text-white-400 font-montserrat
+                           text-base leading-normal hover:text-slate-gray cursor-pointer"
+                      >
+                        <a href={link.link}>{link.name}</a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div
+        className="flex justify-between text-white-400 mt-24 
+      max-sm:flex-col max-sm:items-center"
+      >
+        <div
+          className="flex flex-1 justify-start items-center gap-2 
+        font-montserrat cursor-pointer"
+        >
+          <img
+            src={copyrightSign}
+            alt="copy right sign"
+            width={20}
+            height={20}
+            className="rounded-full"
+          />
+          <p>Copyright. All rights reserved</p>
+        </div>
+        <p className="font-montserrat cursor-pointer"> Terms & Conditions</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
